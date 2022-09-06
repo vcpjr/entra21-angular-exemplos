b4706842598add0d336ef6a06b8f44b0add8caed
VILMAR CESAR PEREIRA JUNIOR <vilmar.junior@prof.sc.senac.br>
2022-09-05 22:19:21 -0300

    Primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'entra21-angular-exemplos';
+}
